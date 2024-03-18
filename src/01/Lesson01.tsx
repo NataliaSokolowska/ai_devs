ba0bb0fa1aa673c6de19215b5e537c329e8ad1c2
NataliaSokolowska <nataliasokolowska@bedeprogramistka.pl>
2024-03-18 20:18:34 +0100

    Add first component

---
+++ src/01/Lesson01.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { ResponseInterface, run } from "../00/Config";
+import { TASK_NAME_01 } from "../00/Config.constants";
+
+const HelloApiComponent = () => {
+  const [response, setResponse] = useState<ResponseInterface>({
+    result: {
+      code: 0,
+      msg: "",
+      note: "",
+    },
+    answer: "",
+  });
+
+  useEffect(() => {
+    run(TASK_NAME_01)
+      .then(({ result, answer }) => setResponse({ result, answer }))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <>
+      <h2>Lesson 01</h2>
+      <p>
+        Response: {response.result.msg}, {response.result.note}
+      </p>
+      <p>Answer: {response.answer}</p>
+    </>
+  );
+};
+
+export default HelloApiComponent;
