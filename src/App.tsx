a1d3ea23ad1d6993079001352a9ed1e9e1d303a1
NataliaSokolowska <nataliasokolowska@bedeprogramistka.pl>
2024-03-18 20:17:58 +0100

    Add project for ai_devs course

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Lesson01 from "./01/Lesson01";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>AI devs</h1>
+      <Lesson01 />
+    </>
+  );
+}
+
+export default App;
